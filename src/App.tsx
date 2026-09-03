16ff829d30df97610109566e7cf1d8fadfeb24ce
Foysal Hossien <foysalhossien77@gmail.com>
2026-09-03 23:41:58 +0600

    feat(react): learn component props

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ function App() {
     <main className="min-h-screen bg-slate-900">
       <Header />
       <section className="p-6 grid gap-4 md:grid-cols-3">
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
+        <ProfileCard name="Robin" role="Web Developer" experience={3} />
+        <ProfileCard name="Rasel" role="Digital Marketer" experience={5} />
+        <ProfileCard name="Sadik" role="Motion Designer" experience={2} />
       </section>
     </main>
   );
--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,9 +1,19 @@
-const ProfileCard = () => {
+interface ProfileCardProps {
+  name: string;
+  role: string;
+  experience: number;
+}
+
+const ProfileCard = ({ name, role, experience }: ProfileCardProps) => {
   return (
     <article className="rounded-xl bg-slate-800 p-6 text-white">
-      <h2 className="text-2xl font-semibold">Foysal</h2>
+      <h2 className="text-2xl font-semibold">{name}</h2>
 
-      <p className="mt-2 text-slate-300">Frontend Developer</p>
+      <p className="mt-2 text-slate-300">{role}</p>
+
+      <p className="mt-1 text-sm text-slate-400">
+        Experience: {experience} years
+      </p>
 
       <button className="mt-4 rounded-lg bg-blue-600 px-4 py-2 font-medium">
         View Profile
