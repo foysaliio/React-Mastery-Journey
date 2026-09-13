e21d024aacdba868069621543a204b7bfcb34078
Foysal Hossien <foysalhossien77@gmail.com>
2026-09-14 02:34:08 +0600

    feat(react): learn lifting state up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import ValidationForm from "./Components/ValidationForm";
+import { useState } from "react";
+import NameInput from "./Components/NameInput";
+import NamePreview from "./Components/NamePreview";
 
 function App() {
+  const [name, setName] = useState<string>("");
   return (
     <main className="min-h-screen bg-slate-950 p-8">
-      <ValidationForm />
+      <div className="mx-auto max-w-xl space-y-6">
+        <NameInput name={name} onNameChange={setName} />
+
+        <NamePreview name={name} />
+      </div>
     </main>
   );
 }
