e808c30d8ea1980dc75c700763d89472ca70ec4f
Foysal Hossien <foysalhossien77@gmail.com>
2026-09-15 02:48:26 +0600

    feat(react): learn designing state shape

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import ProductSection from "./Components/ProductSection";
-import SearchBox from "./Components/SearchBox";
+import UserProfileForm from "./Components/UserProfileForm";
 
 function App() {
   return (
     <main className="min-h-screen bg-slate-950 p-8">
-      <div className="mx-auto max-w-xl space-y-6">
-        <SearchBox />
-        <ProductSection />
-      </div>
+      <UserProfileForm />
     </main>
   );
 }
