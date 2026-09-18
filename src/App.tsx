8ed74c4c8ca8e7e774663a54a0e9f560d71404b5
Foysal Hossien <foysalhossien77@gmail.com>
2026-09-19 04:53:17 +0600

    feat(react): learn useRef basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import EffectMistakes from "./Components/EffectMistakes";
+import RefCounter from "./Components/RefCounter";
 
 function App() {
   return (
     <main className="min-h-screen bg-slate-950 p-8">
-      <EffectMistakes />
+      <RefCounter />
     </main>
   );
 }
