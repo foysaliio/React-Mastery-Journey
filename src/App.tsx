bbecfe199b1c49489a6626148c98b8f9fea44d04
Foysal Hossien <foysalhossien77@gmail.com>
2026-09-18 17:39:24 +0600

    feat(react): cancel fetch requests with AbortController

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import UserList from "./Components/UserList";
+import UserSearch from "./Components/UserSearch";
 
 function App() {
   return (
     <main className="min-h-screen bg-slate-950 p-8">
-      <UserList />
+      <UserSearch />
     </main>
   );
 }
