5b45b7600e5d844f0c977cbd5ca599ceb33e7bd3
Foysal Hossien <foysalhossien77@gmail.com>
2026-09-19 20:36:28 +0600

    feat(react): scroll DOM elements with refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import FocusInput from "./Components/FocusInput";
+import ScrollToSection from "./Components/ScrollToSection";
 
 function App() {
   return (
     <main className="min-h-screen bg-slate-950 p-8">
-      <FocusInput />
+      <ScrollToSection />
     </main>
   );
 }
